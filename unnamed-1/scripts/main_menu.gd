f0603b0a08007caa54240baa290d320257a0870c
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-07-14 20:38:41 +0700

    - Cập nhập script main_menu và pause_menu. - Cập nhập scene PauseMenu.

--- unnamed-1/scripts/main_menu.gd
+++ unnamed-1/scripts/main_menu.gd
@@ -1,11 +1,9 @@
 extends Control
 
-
-@export var main_scene: PackedScene # scene được load khi nhấn nút "BẮT ĐẦU"
-
+const MAIN_SCENE_PATH := "res://scenes/Main.tscn" # scene chính của game
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_packed(main_scene) # Load scene được chỉ định trong biến main_scene
+	get_tree().change_scene_to_file(MAIN_SCENE_PATH) # Load scene được chỉ định trong biến MAIN_SCENE_PATH
 
 
 func _on_exit_button_pressed() -> void:
--- unnamed-1/scripts/pause_menu.gd
+++ unnamed-1/scripts/pause_menu.gd
@@ -1,1 +1,35 @@
 extends Control
+
+const MAIN_MENU_PATH := "res://scenes/MainMenu.tscn"
+
+func _ready() -> void:
+	# Đảm bảo menu bị ẩn khi game bắt đầu
+	hide()
+
+
+func _input(event: InputEvent) -> void:
+	# Kiểm tra nếu nhấn phím ESC (ui_cancel)
+	if event.is_action_pressed("ui_cancel"):
+		if get_tree().paused:
+			resume()
+		else:
+			pause()
+
+
+func pause() -> void:
+	get_tree().paused = true # Dừng toàn bộ game
+	show() # Hiện menu
+
+
+func resume() -> void:
+	get_tree().paused = false # Chạy lại game
+	hide() # Ẩn menu
+
+
+func _on_resume_button_pressed() -> void:
+	resume()
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().paused = false # tắt pause trước chuyển scene để tránh lỗi
+	get_tree().change_scene_to_file(MAIN_MENU_PATH) # Load scene được chỉ định trong biến MAIN_MENU_PATH
